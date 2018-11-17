aec5b9e5d346c397d933a0ec2c51e998a11a9aff
gillespirp <44880321+gillespirp@users.noreply.github.com>
2018-11-16 23:24:38 -0500

    Update reproductor2.py

--- programa2/reproductor2.py
+++ programa2/reproductor2.py
@@ -1,40 +1,67 @@
 from tkinter import*
 from tkinter import ttk
+import pygame
+from pygame import mixer
+import sys
+from tinytag import tinytag, TinyTag
+
+
 root=Tk()
 root.title("reproductor")
 root.geometry('300x100')
 root.iconbitmap(r'C:\Users\xgian\Desktop\programa\botones\favicon.ico')
+mixer.init()
+
+
 
 #funciones
-def play_btn():
+def playboton():
+    mixer.music.load(fr"C:\Users\xgian\Desktop\programa\canciones\Laura Pergolizzi - Lost on You\Lost On You.mp3")
+    mixer.music.play()
+paused=FALSE
+def pausaboton():
+    mixer.music.pause()
+
+
+
+def atrasboton():
     print("hola")
+def siguienteboton():
+    print("holaa")
 
 
 #botones
 photoatras=PhotoImage(file='atras.png')
-atras=Button(root, image=photoatras, command=play_btn)
+atras=Button(root, image=photoatras, command=atrasboton)
 atras.pack(side=LEFT)
 
+
 photoplay=PhotoImage(file='play.png')
-play=Button(root, image=photoplay, command=play_btn)
+play=Button(root, image=photoplay, command=playboton)
 play.pack(side=LEFT)
 
+
 photopausa=PhotoImage(file='pausa.png')
-pausa=Button(root, image=photopausa, command=play_btn)
+pausa=Button(root, image=photopausa, command=pausaboton)
 pausa.pack(side=LEFT)
 
+
 photosiguiente=PhotoImage(file='siguiente.png')
-siguiente=Button(root, image=photosiguiente, command=play_btn)
+siguiente=Button(root, image=photosiguiente, command=siguienteboton)
 siguiente.pack(side=LEFT)
 
+
 photoshuffle=PhotoImage(file='shuffle.png')
-shuffle=Button(root, image=photoshuffle, command=play_btn)
+shuffle=Button(root, image=photoshuffle, command=siguienteboton)
 shuffle.pack(side=LEFT)
 
 photosonido=PhotoImage(file='sonido.png')
-sonido=Button(root, image=photosonido, command=play_btn)
+sonido=Button(root, image=photosonido, command=siguienteboton)
 sonido.pack(side=LEFT)
 
+buscador=Entry(root,width=30)
+buscador.pack(side=LEFT)
+buscador.pack(side=BOTTOM)
 
 
 root.mainloop()
